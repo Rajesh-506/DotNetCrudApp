c0f0fedcdba73cce981ff29450fe6bac2fafc4de
OmkaramRR <rajeshwara506@gmail.com>
2025-02-15 10:19:43 +0530

    Created Project Template

---
+++ DotNetCrudApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotNetCrudApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
