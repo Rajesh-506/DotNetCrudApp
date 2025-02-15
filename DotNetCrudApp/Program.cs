2667657f45f73f098d049c110b2981c6a548b799
OmkaramRR <rajeshwara506@gmail.com>
2025-02-15 11:19:53 +0530

    Updated Program.cs file

--- DotNetCrudApp/Program.cs
+++ DotNetCrudApp/Program.cs
@@ -1,29 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("ProductsDB"));
 builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+app.UseRouting();
+app.UseEndpoints(endpoints =>
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Product}/{action=Index}/{id?}");
+});
+app.Run();
+
+public class Product
+{
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public decimal Price { get; set; }
 }
 
-app.UseHttpsRedirection();
-app.UseRouting();
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Product> Products { get; set; }
+}
 
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
-app.Run();
+public class ProductController : Controller
+{
+    private readonly AppDbContext _context;
+    public ProductController(AppDbContext context) => _context = context;
+    
+    public IActionResult Index() => View(_context.Products.ToList());
+    public IActionResult Create() => View();
+    [HttpPost]
+    public IActionResult Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(product);
+    }
+}
--- DotNetCrudApp/obj/Debug/net9.0/DotNetCrudApp.AssemblyInfo.cs
+++ DotNetCrudApp/obj/Debug/net9.0/DotNetCrudApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetCrudApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+032b57d9b8f33e06c640fe2ee45f8902899db5b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetCrudApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetCrudApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
